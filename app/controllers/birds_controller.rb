2b3eb911085ce52aa1307e312bda91a26e02d817
Michael Maggs <michaelmaggs@gmail.com>
2019-07-26 17:51:16 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -3,4 +3,8 @@ class BirdsController < ApplicationController
     @birds = Bird.all
     render json: @birds only[:id, :name, :species]
   end
+
+  def show
+    @bird = Bird.find(id: params[:id])
+    render json: @bird.slice(:id, :name, :species)
 end
